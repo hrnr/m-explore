e124cd9b81ef0d1441ef930b672533260510c1a2
Jiri Horner <laeqten@gmail.com>
2016-03-12 22:24:27 +0100

    map_merge: use constant iterators for estimate_transform

--- map_merge/src/map_merge.cpp
+++ map_merge/src/map_merge.cpp
@@ -148,7 +148,7 @@ void MapMerging::mapMerging()
     std::lock_guard<boost::shared_mutex> lock(merging_mutex_);
     occupancy_grid_utils::combineGrids(grid_view_.cbegin(), grid_view_.cend(),
                                        merged_map_);
-    combine_grids::estimateGridTransform(grid_view_.begin(), grid_view_.end());
+    combine_grids::estimateGridTransform(grid_view_.cbegin(), grid_view_.cend());
   }
 
   ROS_DEBUG("all maps merged, publishing");
