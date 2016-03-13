43eae5414d74a387e02ff0b4cfcc816bbfbc566c
Jiri Horner <laeqten@gmail.com>
2016-03-13 12:30:19 +0100

    estimate_trasform: new features matcher
    new features matcher for our purposes. Uses limited set of affine transformations: translation, rotation and scale. We will use it instead of matchers from opencv stitching pipeline since they use perspective transformation, which is not reflecting physical contrains on map merging.

---
+++ map_merge/include/combine_grids/features_matcher.h
@@ -0,0 +1,66 @@
+/*********************************************************************
+ *
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2015-2016, Jiri Horner.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the author nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************/
+
+#ifndef FEATURES_MATCHER_H_
+#define FEATURES_MATCHER_H_
+
+#include <opencv2/stitching/detail/matchers.hpp>
+
+namespace combine_grids
+{
+namespace internal
+{
+/** @brief Features matcher similar to cv::detail::BestOf2NearestMatcher which
+finds two best matches for each feature and leaves the best one only if the
+ratio between descriptor distances is greater than the threshold match_conf.
+
+Unlike cv::detail::BestOf2NearestMatcher this matcher is using affine
+transformation to estimate homography.
+
+@sa cv::detail::FeaturesMatcher cv::detail::BestOf2NearestMatcher
+ */
+class AffineBestOf2NearestMatcher : public cv::detail::BestOf2NearestMatcher
+{
+protected:
+  void match(const cv::detail::ImageFeatures &features1,
+             const cv::detail::ImageFeatures &features2,
+             cv::detail::MatchesInfo &matches_info);
+};
+
+}  // namespace internal
+}  // namespace combine_grids
+
+#endif  // FEATURES_MATCHER_H_
